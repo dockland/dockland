226821f7b9a08b6f6f748304d64d1a849a22383c
Clément Michel <m1ch3lcl@gmail.com>
2018-01-30 22:28:12 +0100

    add comments for config variables

--- src/config.js
+++ src/config.js
@@ -1,16 +1,24 @@
 module.exports = {
+  // express server exposing webhooks and status API
   manager: {
+    // domain name for manager
     domain: process.env.MANAGER_SUBDOMAIN || process.env.PROXY_DOMAIN,
-    path: process.env.MANAGER_PATH || '',
+    // port on which manager is listening, proxy will pass requests to this port
     port: process.env.MANAGER_PORT || 5000,
+    // use truthy value to bypass payload validation when receiving webhooks
+    // see (https://docs.travis-ci.com/user/notifications/#Verifying-Webhook-requests)
     trustWebhooks: process.env.TRUST_WEBHOOKS
   },
   proxy: {
+    // base domain name for proxy, deployed apps will be listening on [repoName].[domain]
     domain: process.env.PROXY_DOMAIN,
+    // port for https connections
     sslPort: process.env.HTTPS_PROXY_PORT,
+    // port for http connection
     port: process.env.PROXY_PORT
   },
   docker: {
+    // root folder to store docker-compose.yml files
     root: process.env.DOCKER_ROOT || 'projects'
   }
 }
